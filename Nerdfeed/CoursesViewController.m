0b3005a5ac5448d9d72afe33d69c132b2a9bda6c
krzychukula <krzychukula@gmail.com>
2014-10-28 13:03:06 +0100

    page 409. make request

--- Nerdfeed/CoursesViewController.m
+++ Nerdfeed/CoursesViewController.m
@@ -25,8 +25,9 @@
         _session = [NSURLSession sessionWithConfiguration:config
                                                  delegate:nil
                                             delegateQueue:nil];
+        [self fetchFeed];
     }
-    NSLog(@"initWithStyle");
+    
     return self;
 }
 
@@ -39,6 +40,23 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     return nil;
+}
+
+- (void)fetchFeed
+{
+    NSString *requestString = @"http://bookapi.bignerdranch.com/courses.json";
+    NSURL *url = [NSURL URLWithString:requestString];
+    NSURLRequest *req = [NSURLRequest requestWithURL:url];
+    
+    NSURLSessionDataTask *dataTask = [self.session dataTaskWithRequest:req
+                                                     completionHandler:^(NSData *data, NSURLResponse *response, NSError *error){
+                                                         
+                                                         NSString *json = [[NSString alloc] initWithData:data
+                                                                                                encoding:NSUTF8StringEncoding];
+                                                         NSLog(@"%@", json);
+                                                         
+                                                     }];
+    [dataTask resume];
 }
 
 
