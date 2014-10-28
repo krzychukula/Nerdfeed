c7f692717aa445330760c7b74bf239c1fc8197b9
krzychukula <krzychukula@gmail.com>
2014-10-28 10:47:37 +0100

    page 405. use storyboards for table

---
+++ Nerdfeed/CoursesViewController.h
@@ -0,0 +1,11 @@
+//
+//  CoursesViewController.h
+//  Nerdfeed
+//
+//  Created by Krzysztof Kula on 28/10/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+@interface CoursesViewController : UITableViewController
+@end
---
+++ Nerdfeed/CoursesViewController.m
@@ -0,0 +1,25 @@
+//
+//  CoursesViewController.m
+//  Nerdfeed
+//
+//  Created by Krzysztof Kula on 28/10/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "CoursesViewController.h"
+
+@implementation CoursesViewController
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    NSLog(@"get num of rows");
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
+
+
+@end
