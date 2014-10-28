535bf4fd0d27b3334e1842f53360e09c1d1e5c13
krzychukula <krzychukula@gmail.com>
2014-10-28 10:00:31 +0100

    XCode files

---
+++ Nerdfeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nerdfeed
+//
+//  Created by Krzysztof Kula on 28/10/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
